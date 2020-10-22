ae7901a7e1e51634199aee395aa8e1c1eb7a10d9
zainab052 <32038609+zainab052@users.noreply.github.com>
2020-10-22 10:27:24 +0300

    updating to fix encoding error

--- data_extract_task3.py
+++ data_extract_task3.py
@@ -1,8 +1,10 @@
 import requests
-from lxml import html
+from lxml import html, etree
 import json
 import sqlalchemy as db
-from sqlalchemy import create_engine, Table, Column, Integer, VARCHAR, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
+from sqlalchemy.orm import sessionmaker
 import pandas as pd
 
 
@@ -39,56 +41,62 @@ headers1 = {
     'cookie': 'session=7tn165u24eetjrqhd2oot0nidbec9johl6r8aesm6fk3o4pv;',
 }
 
+
 data = []
 for x in range(1, 2):
-    
-    response = requests.get('https://gensdeconfiance.com/api/v2/classified?category=realestate__rent&orderColumn=displayDate&orderDirection=DESC&rootLocales\[\]=en&rootLocales\[\]=fr&hidden=true&page=' + str(x) + '&dateTo=@1602677994&itemsPerPage=50', headers=headers)
-    json_file = json.loads(response.text)
+    myparser = etree.HTMLParser(encoding="utf-8")
+    response = requests.get('https://gensdeconfiance.com/api/v2/classified?category=realestate__rent&orderColumn=displayDate&orderDirection=DESC&rootLocales\[\]=en&rootLocales\[\]=fr&hidden=true&page=' + str(x) + '&dateTo=@1602677994&itemsPerPage=100', headers=headers)
+    json_file = json.loads(response.content)
 
     for x in json_file:
 
-        if x['title']:
-            title = str(x['title'])
-        else:
-            title = ''
-
-        if x['type']:
+        if 'type' in x:
             types = str(x['type'])
         else:
             types = ''
 
-        if x['category']:
+        if 'category' in x:
             category = str(x['category'])
         else:
             category = ''
 
-        if x['city']:
+        if 'city' in x:
             city = str(x['city'])
         else:
             city = ''
 
-        if x['zip']:
+        if 'price' in x:
+            price = str(x['price'])
+        else:
+            price = ''
+
+        if 'zip' in x:
             zips = str(x['zip'])
         else:
             zips = ''
 
-        if x['description']:
-            description = str(x['description'])
-        else:
-            description = ''
-
         url = 'https://gensdeconfiance.com/fr/annonce/'+ str(x['slug'])
         response1 = requests.get(url, headers=headers1)
-        doc = html.fromstring(response1.content)
+        doc = html.fromstring(response1.content, parser=myparser)
 
 
 
         div = doc.xpath('//div[@id="content"]/div[@id="contentInner"]')
         for d in div:
+            title = ''.join(d.xpath('//h1[@id="post-title"]/text()'))
+            title = title.replace('  ', '')
             chambre = ''.join(d.xpath('//div[@class="col-md-8"]/table[@class="table text-bold bg-default br"]/tr/td[@id="ad-extra-fields-nb_rooms"]/text()'))
+            chambre = chambre.replace('\n', '')
+            chambre = chambre.replace('  ', '')
             surface = ''.join(d.xpath('//div[@class="col-md-8"]/table[@class="table text-bold bg-default br"]/tr/td[@id="ad-extra-fields-nb_square_meters"]/text()'))
+            surface = surface.replace('\n', '')
+            surface = surface.replace('  ', '')
             phone = ''.join(d.xpath('//div[@class="m-b-md"]/a/text()'))
-            price = ''.join(d.xpath('//div[@class="price-table__value"]/text()'))
+            phone = phone.replace('\n', '')
+            phone.replace('  ', '')
+            description = ''.join(d.xpath('//div[@id="ad-description"]/text()', encoding="utf-8"))
+            description = description.replace('\n', '')
+            description = description.replace('  ', '')
 
             datas = {
             'url' : url,
@@ -103,38 +111,45 @@ for x in range(1, 2):
             'surface': surface,
             'telephone' : phone,
             }
+
         data.append(datas)
-        print(type(chambre), chambre)
+        
 
 
-# engine = db.create_engine('mysql://root:24M@y!996@localhost/gensdeconfiance')
-# connection = engine.connect()
-# metadata = db.MetaData()
+engine = db.create_engine('mysql://root:24M@y!996@localhost/gensdeconfiance', echo=True, convert_unicode=False, encoding="utf-8")
+Base = declarative_base()
+
+class RoomDetails(Base):
+   __tablename__ = 'room_details'
+   
+   id = Column(Integer, primary_key = True)
+   url = Column(String(5000))
+   title = Column(String(1000))
+   types = Column(String(500))
+   categorie = Column(String(1000))
+   ville = Column(String(500))
+   cp = Column(String(500))
+   prix = Column(String(500))
+   description = Column(String(5000))
+   chambres = Column(String(500))
+   surface = Column(String(500))
+   telephone = Column(String(500))
 
 
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind = engine)
+session = Session()
 
-# census = db.Table('details', metadata, autoload=True, autoload_with=engine)
-# query = census.insert(), data
-# connection.execute(query)
 
-meta = MetaData()
+for d in data: 
+    print(d)  
+    try:
+        d1 = RoomDetails(url = d['url'], title = d['title'], types=d['type'], categorie=d['categorie'], ville=d['ville'], cp=d['cp'], 
+        prix=d['prix'], description=d['description'], chambres=d['chambres'], surface=d['surface'], telephone=d['telephone'])
+        session.add(d1)
+        session.commit()
+        print("Success")
+    except:
+        print("An error occured")
 
-test = Table(
-   'test', meta, 
-   Column('id', Integer, primary_key = True), 
-   Column('url', VARCHAR(1000)), 
-   Column('title', VARCHAR(1000)),
-   Column('type', VARCHAR(500)), 
-   Column('categorie', VARCHAR(1000)),
-   Column('ville', VARCHAR(500)),
-   Column('cp', VARCHAR(500)),
-   Column('prix', VARCHAR(500)),
-   Column('description', VARCHAR(5000)),
-   Column('chambres', VARCHAR(500)),
-   Column('surface', VARCHAR(500)),
-   Column('telephone', VARCHAR(500)),
-)
-
-meta.create_all(engine)
-conn = engine.connect()
-conn.execute(students.insert(), data)
+ 
